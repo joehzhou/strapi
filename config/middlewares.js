52ca5bd760505c42a62c60790331a7b1dc4c6ce4
Joe Zhou <joe@themartec.com>
2025-03-19 22:01:44 +1100

    Update CORS, and package versions.

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,14 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:3000', 'http://localhost:1337', 'http://localhost:5173', '*']
+    }
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
